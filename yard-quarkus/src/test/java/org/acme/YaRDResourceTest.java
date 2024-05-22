b2e74420d6200ff12f6425be15e8fc71382c48b9
rikkola <toni@rikkola.net>
2024-05-22 18:00:32 +0300

    remove dead code

--- yard-quarkus/src/test/java/org/acme/YaRDResourceTest.java
+++ yard-quarkus/src/test/java/org/acme/YaRDResourceTest.java
@@ -6,13 +6,13 @@ import org.drools.util.IoUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class YaRDResourceTest {
+
     @Test
     void testHelloEndpoint() throws IOException {
         final String yard = new String(IoUtils.readBytesFromInputStream(this.getClass().getResourceAsStream("/simplified-ticket-score.json"), true));
@@ -32,16 +32,4 @@ class YaRDResourceTest {
                 .statusCode(200)
                 .body(is("{\"Bronze Complete\":true,\"Silver Complete\":false,\"Gold Complete\":false,\"Level\":\"Bronze\"}"));
     }
-
-    @Test
-    void testHello() {
-        HashMap<String, Object> map = new HashMap<>();
-
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello from Quarkus REST"));
-    }
-
 }
