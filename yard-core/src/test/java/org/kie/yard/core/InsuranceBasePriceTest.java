d4f5a8c92a5d36952959e1bfb46afd8cd5f1070b
Rikkola <toni@rikkola.net>
2025-07-10 13:23:24 +0300

    Remove dead YARD folders

--- yard-core/src/test/java/org/kie/yard/core/InsuranceBasePriceTest.java
+++ yard-core/src/test/java/org/kie/yard/core/InsuranceBasePriceTest.java
@@ -25,33 +25,33 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InsuranceBasePriceTest
-        extends TestBase {
+    extends TestBase {
 
-    private static final String FILE_NAME = "/insurance-base-price.yml";
+  private static final String FILE_NAME = "/insurance-base-price.yml";
 
-    @Test
-    public void testScenario1() throws Exception {
-        final String CTX = """
-                {
-                  "Age": 47,
-                  "Previous incidents?": false
-                }
-                """;
-        Map<String, Object> outputJSONasMap = evaluate(CTX, FILE_NAME);
-        assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Base price", 500);
-        assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Downpayment", 50);
-    }
+  @Test
+  public void testScenario1() throws Exception {
+    final String CTX = """
+        {
+          "Age": 47,
+          "Previous incidents?": false
+        }
+        """;
+    Map<String, Object> outputJSONasMap = evaluate(CTX, FILE_NAME);
+    assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Base price", 500);
+    assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Downpayment", 50.0);
+  }
 
-    @Test
-    public void testScenario2() throws Exception {
-        final String CTX = """
-                {
-                  "Age": 19,
-                  "Previous incidents?": true
-                }
-                """;
-        Map<String, Object> outputJSONasMap = evaluate(CTX, FILE_NAME);
-        assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Base price", 1000);
-        assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Downpayment", 70);
-    }
+  @Test
+  public void testScenario2() throws Exception {
+    final String CTX = """
+        {
+          "Age": 19,
+          "Previous incidents?": true
+        }
+        """;
+    Map<String, Object> outputJSONasMap = evaluate(CTX, FILE_NAME);
+    assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Base price", 1000);
+    assertThat(outputJSONasMap).hasFieldOrPropertyWithValue("Downpayment", 70.0);
+  }
 }
